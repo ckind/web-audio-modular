36f70dc7db56610876eda6d3b7a5ade13b9ab5b4
ckind <carlo.a.kind@gmail.com>
2026-01-19 02:58:04 -0500

    update scale module to use worklet

--- src/classes/audio-modules/ScaleModule.ts
+++ src/classes/audio-modules/ScaleModule.ts
@@ -3,24 +3,34 @@ import type { AudioModuleType } from "@/classes/audio-modules/AudioModule";
 import ModuleInput from "@/classes/ModuleInput";
 import ModuleOutput from "@/classes/ModuleOutput";
 import * as Tone from "tone";
+import ScaleWorkletNode from "@/classes/audio-nodes/ScaleWorkletNode";
 
 type ScaleModuleOptions = {
-  min: number;
-  max: number;
+  inputMin: number;
+  inputMax: number;
+  outputMin: number;
+  outputMax: number;
 };
 
 const getDefaultOptions = (): ScaleModuleOptions => ({
-  min: 0,
-  max: 1,
+  inputMin: -1,
+  inputMax: 1,
+  outputMin: 0,
+  outputMax: 1,
 });
 
 export default class ScaleModule extends AudioModule<ScaleModuleOptions> {
-  private _scaleNode: Tone.Scale;
+  private _scaleNode: ScaleWorkletNode;
 
   constructor(id: string, options?: ScaleModuleOptions) {
     super(id, options ?? getDefaultOptions());
 
-    this._scaleNode = new Tone.Scale(this._options.min, this._options.max);
+    this._scaleNode = new ScaleWorkletNode({
+      inputMin: this._options.inputMin,
+      inputMax: this._options.inputMax,
+      outputMin: this._options.outputMin,
+      outputMax: this._options.outputMax,
+    });
 
     this._outputs = [new ModuleOutput("scale-signal-output", this._scaleNode)];
     this._inputs = [new ModuleInput("scale-signal-input", this._scaleNode)];
@@ -31,13 +41,21 @@ export default class ScaleModule extends AudioModule<ScaleModuleOptions> {
   }
 
   updateOptions(options: Partial<ScaleModuleOptions>): void {
-    if (options.min !== undefined) {
-      this._scaleNode.min = options.min;
-      this._options.min = options.min;
+    if (options.inputMin !== undefined) {
+      this._scaleNode.inputMin.value = options.inputMin;
+      this._options.inputMin = options.inputMin;
     }
-    if (options.max !== undefined) {
-      this._scaleNode.max = options.max;
-      this._options.max = options.max;
+    if (options.inputMax !== undefined) {
+      this._scaleNode.inputMax.value = options.inputMax;
+      this._options.inputMax = options.inputMax;
+    }
+    if (options.outputMin !== undefined) {
+      this._scaleNode.outputMin.value = options.outputMin;
+      this._options.outputMin = options.outputMin;
+    }
+    if (options.outputMax !== undefined) {
+      this._scaleNode.outputMax.value = options.outputMax;
+      this._options.outputMax = options.outputMax;
     }
   }
 
