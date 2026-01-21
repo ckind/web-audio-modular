7436bb76eb4e704ec7bbf62756cdbd7c1e5f0caa
ckind <carlo.a.kind@gmail.com>
2026-01-20 21:14:19 -0500

    add message input and output to scale

--- src/classes/audio-modules/ScaleModule.ts
+++ src/classes/audio-modules/ScaleModule.ts
@@ -2,8 +2,11 @@ import AudioModule from "@/classes/audio-modules/AudioModule";
 import type { AudioModuleType } from "@/classes/audio-modules/AudioModule";
 import ModuleInput from "@/classes/ModuleInput";
 import ModuleOutput from "@/classes/ModuleOutput";
-import * as Tone from "tone";
+import MessageOutputNode from "@/classes/MessageOutputNode";
+import MessageInputNode from "@/classes/MessageInputNode";
 import ScaleWorkletNode from "@/classes/audio-nodes/ScaleWorkletNode";
+import * as Tone from "tone";
+import type { MessageBusDataType } from "@/types/connectionTypes";
 
 type ScaleModuleOptions = {
   inputMin: number;
@@ -19,8 +22,23 @@ const getDefaultOptions = (): ScaleModuleOptions => ({
   outputMax: 1,
 });
 
+function scaleValue(
+  value: number,
+  inputMin: number,
+  inputMax: number,
+  outputMin: number,
+  outputMax: number,
+): number {
+  const scaled =
+    ((value - inputMin) / (inputMax - inputMin)) * (outputMax - outputMin) +
+    outputMin;
+  return scaled;
+}
+
 export default class ScaleModule extends AudioModule<ScaleModuleOptions> {
   private _scaleNode: ScaleWorkletNode;
+  private _messageInputNode: MessageInputNode;
+  private _messageOutputNode: MessageOutputNode;
 
   constructor(id: string, options?: ScaleModuleOptions) {
     super(id, options ?? getDefaultOptions());
@@ -32,8 +50,11 @@ export default class ScaleModule extends AudioModule<ScaleModuleOptions> {
       outputMax: this._options.outputMax,
     });
 
-    this._outputs = [new ModuleOutput("scale-signal-output", this._scaleNode)];
-    this._inputs = [new ModuleInput("scale-signal-input", this._scaleNode)];
+    this._messageInputNode = new MessageInputNode(this.messageInputCallback.bind(this));
+    this._messageOutputNode = new MessageOutputNode();
+
+    this._inputs = [new ModuleInput("signal-input", this._scaleNode), new ModuleInput("message-input", this._messageInputNode)];
+    this._outputs = [new ModuleOutput("signal-output", this._scaleNode), new ModuleOutput("message-output", this._messageOutputNode)];
   }
 
   get type(): AudioModuleType {
@@ -56,6 +77,20 @@ export default class ScaleModule extends AudioModule<ScaleModuleOptions> {
     if (options.outputMax !== undefined) {
       this._scaleNode.outputMax.value = options.outputMax;
       this._options.outputMax = options.outputMax;
+    }
+  }
+
+  messageInputCallback(time: number, data?: MessageBusDataType): void {
+    const num = new Number(data);
+    if (!isNaN(num.valueOf())) {
+      const scaledValue = scaleValue(
+        num.valueOf(),
+        this._options.inputMin,
+        this._options.inputMax,
+        this._options.outputMin,
+        this._options.outputMax
+      );
+      this._messageOutputNode.scheduleMessage(time, scaledValue);
     }
   }
 
