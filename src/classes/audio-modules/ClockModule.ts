c9b15804c0855b3a6d14903fe06d10f88dbde455
ckind <carlo.a.kind@gmail.com>
2026-01-20 21:49:16 -0500

    update clock module to use tone.js clock instead of transport; add input for controlling bpm

--- src/classes/audio-modules/ClockModule.ts
+++ src/classes/audio-modules/ClockModule.ts
@@ -15,35 +15,38 @@ const getDefaultOptions = (): ClockModuleOptions => ({
   bpm: 120,
 });
 
+function bpmToHz(bpm: number): number {
+  return bpm / 60;
+}
+
 export default class ClockModule extends AudioModule<ClockModuleOptions> {
-  private _intervalSeconds: number;
-  private _running: boolean = false;
-  private _eventId: number;
+  private _startStopInput: MessageInputNode;
+  private _bpmInput: MessageInputNode;
   private _counter: number = 0;
 
   private _messageOutput: MessageOutputNode;
 
+  private _clock: Tone.Clock;
+
   constructor(id: string, options?: ClockModuleOptions) {
     super(id, options ?? getDefaultOptions());
 
-    this._intervalSeconds = 60 / this._options.bpm;
-    this._eventId = Tone.getTransport().scheduleRepeat(
-      (time: number) => this._clockCallback(time),
-      this._intervalSeconds,
+    this._clock = new Tone.Clock(
+      this._clockCallback.bind(this),
+      bpmToHz(this._options.bpm),
     );
+
+    this._clock.start();
+
+    this._startStopInput = new MessageInputNode(this.toggleRunning.bind(this));
+    this._bpmInput = new MessageInputNode(this.bpmInputCallback.bind(this));
 
     this._messageOutput = new MessageOutputNode();
     this._outputs = [new ModuleOutput("clock-output", this._messageOutput)];
     this._inputs = [
-      new ModuleInput(
-        "clock-start-stop",
-        new MessageInputNode((time, data) => {
-          this.toggleRunning(time, data);
-        }),
-      ),
+      new ModuleInput("clock-start-stop", this._startStopInput),
+      new ModuleInput("clock-bpm", this._bpmInput),
     ];
-
-    this._running = true;
   }
 
   get type(): AudioModuleType {
@@ -51,31 +54,35 @@ export default class ClockModule extends AudioModule<ClockModuleOptions> {
   }
 
   private _clockCallback(time: number) {
-    if (this._running) {
-      this._counter++;
-      this._messageOutput.scheduleMessage(time, this._counter);
-    }
+    this._counter++;
+    this._messageOutput.scheduleMessage(time, this._counter);
   }
 
   updateOptions(options: Partial<ClockModuleOptions>): void {
     if (options.bpm !== undefined) {
       this._options.bpm = options.bpm;
-      this._intervalSeconds = 60 / this._options.bpm;
-      // rescheduled repeated event
-      // todo: sample accurate way to do this?
-      Tone.getTransport().clear(this._eventId);
-      this._eventId = Tone.getTransport().scheduleRepeat(
-        (time: number) => this._clockCallback(time),
-        this._intervalSeconds,
-      );
+      this._clock.frequency.value = bpmToHz(this._options.bpm);
     }
   }
 
   toggleRunning(time: number, data?: MessageBusDataType): void {
-    this._running = !this._running;
+    if (this._clock.state === "stopped") {
+      this._clock.start(time);
+    } else {
+      this._clock.stop(time);
+    }
+  }
+
+  bpmInputCallback(time: number, data?: MessageBusDataType): void {
+    if (typeof data !== "number") {
+      return;
+    }
+
+    this._clock.frequency.value = bpmToHz(data);
   }
 
   dispose(): void {
-    Tone.getTransport().clear(this._eventId);
+    this._clock.stop();
+    this._clock.dispose();
   }
 }
